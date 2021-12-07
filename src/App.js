3cd25cb2380fa1bb6721b81c3eaf57f5be16bc84
Ryan Lee <ryanyinlee@protonmail.com>
2021-12-06 19:01:25 -0800

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://us1.locationiq.com/v1/reverse.php?key=YOUR_ACCESS_TOKEN&lat=LATITUDE&lon=LONGITUDE&format=json
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      queryCity:'',
+      locationObject: {},
+      error: false
+    }
+  }
+
+ 
+ 
+  getlocation = async() => {
+
+    try {
+    let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.queryCity}&format=json`)
+    this.setState({ locationObject: result.data[0] })
+    } catch (error) {
+      console.error(error);
+      this.setState({error: true});
+    }
+  }
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  this.setState({queryCity: event.target.city.value }, this.getlocation);
 }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <form onSubmit ={this.handleSubmit}>
+          <input type="text" placeholder="Enter City Name Here" name="city"/>
+          <button type="submit">Explore!</button>
+        </form>
+        {this.state.locationObject.display_name ? <p>{this.state.locationObject.display_name}</p> : <p>Search for a city.</p>}
+        {this.state.error && <p>There was an error with your request.</p>}
+      </div>
+    )
+  }
+}
