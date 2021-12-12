59fe9ff710249164c317a9bffbc8e67392f43738
Ryan Lee <ryanyinlee@protonmail.com>
2021-12-12 14:30:01 -0800

    finally got weather working

--- src/App.js
+++ src/App.js
@@ -16,17 +16,22 @@ export default class App extends Component {
       queryCity:'',
       locationObject: {},
       error: false
+      
     }
   }
 
+  
+
  
  
-getlocation = async() => {
+getLocation = async() => {
+
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.queryCity}&format=json`
 
     try {
-    let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.queryCity}&format=json`)
+    let result = await axios.get(url);
     this.setState({ locationObject: result.data[0] });
-    this.setState({error: false});
+    this.setState({ error: false});
     } catch (error) {
       console.error(error);
       this.setState({error: true});
@@ -43,15 +48,12 @@ getlocation = async() => {
   }
 
 
-
-
 handleSubmit = (event) => {
   event.preventDefault();
 
-  this.setState({queryCity: event.target.city.value }, this.getlocation);
+  this.setState({queryCity: event.target.city.value }, this.getLocation);
+  
 }
-
-
 
   render() {
     return (
@@ -64,13 +66,20 @@ handleSubmit = (event) => {
           {this.state.error && <Alert variant="danger">There was an error with your request.</Alert>}
           <button type="submit">Explore!</button>
         </Form>
-        <br></br>{this.state.locationObject.display_name ?  <p>{this.state.locationObject.display_name}, Earth <br></br> <br></br> Latitude: {this.state.locationObject.lat} Longitude: {this.state.locationObject.lon}</p> : <p>Search for a city.</p>}
+        <br></br>{this.state.locationObject.display_name ?  <p>{this.state.locationObject.display_name}, Earth <br></br> <br></br> Latitude: {this.state.locationObject.lat} Longitude: {this.state.locationObject.lon}</p>  : <p>Search for a city.</p>}
+        {this.state.locationObject.display_name ?
+        <Weather 
+        weather={this.state.weather}
+        latitude={this.state.locationObject.lat}
+        longitude={this.state.locationObject.lon}
+        /> : <p>Weather here.</p>}
+    
         
-        {/* Put weather here. */}
-        {/* {this.props.weather.length > 0 && <Weather weather ={this.state.weather} />} */}
         {/* Put movies here. */}
-        {this.state.movies.length > 0 && <Movies movie={this.state.movies.map(movie =>)/>}}
-        <Card.Img variant="bottom" width="450px" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.locationObject.lat},${this.state.locationObject.lon}&zoom=10&size=400x400&format=png&maptype=roadmap&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`} alt="map"/>
+        {/* {this.state.movies.length > 0 && <Movies movie={this.state.movies.map(movie =>)/>}} */}
+        {this.state.locationObject.display_name ?
+        <Card.Img variant="bottom" width="450px" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.locationObject.lat},${this.state.locationObject.lon}&zoom=10&size=400x400&format=png&maptype=roadmap`} alt="map"
+        /> : <p></p>}
        
         </Card.Body>
         </Card>
